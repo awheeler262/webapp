cc48c30e173ca48e7ba503d720a2dfe6052e1cbe
Aaron Wheeler <awheeler262@gmail.com>
2026-07-02 09:34:58 -0400

    WEB -- Use NUXT_PUBLIC_API_BASE_URL and clean caches on generate

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/fonts'],
   runtimeConfig: {
     public: {
-      apiBaseUrl: 'http://localhost:3001'
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || ''
     }
   }
 })
