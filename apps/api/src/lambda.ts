e332eea20971df4d8f9777b5f507ba991b662ca1
Aaron Wheeler <awheeler262@gmail.com>
2026-07-07 09:24:26 -0400

    API -- Add lambda function and buildspec.yaml

---
+++ apps/api/src/lambda.ts
@@ -0,0 +1,28 @@
+import serverlessExpress from '@codegenie/serverless-express';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { Handler } from 'aws-lambda';
+import { AppModule } from './app.module';
+import { configureHelmet, configureSecurityHeaders, configureCors, configureValidation } from './app.config';
+
+let cachedHandler: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const expressApp = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+  configureHelmet(app);
+  configureSecurityHeaders(app);
+  app.setGlobalPrefix('api');
+  configureCors(app);
+  configureValidation(app);
+
+  await app.init();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (event, context, callback) => {
+  cachedHandler ??= await bootstrap();
+  return cachedHandler(event, context, callback);
+};
